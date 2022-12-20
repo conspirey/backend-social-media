5f787f3336d2b923cd2e37e19864493c2886a063
mrredo <mrredogt@gmail.com>
2022-12-20 22:06:38 +0200

    d

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/dchest/uniuri v1.2.0
 	github.com/gin-contrib/sessions v0.0.5
 	github.com/gin-gonic/gin v1.8.1
-	github.com/gorilla/websocket v1.5.0
 	github.com/microcosm-cc/bluemonday v1.0.21
 	github.com/yangxikun/gin-limit-by-key v0.0.0-20190512072151-520697354d5f
 	go.mongodb.org/mongo-driver v1.11.1
@@ -20,11 +19,15 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/gomodule/redigo v2.0.0+incompatible // indirect
+	github.com/googollee/go-socket.io v1.6.2 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
--- routes/ws.go
+++ routes/ws.go
@@ -2,45 +2,34 @@ package routes
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
+	socketio "github.com/googollee/go-socket.io"
+
 )
 
 func LoadWebSocket(r *gin.Engine) {
-	upgrader := websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-		CheckOrigin:     func(r *http.Request) bool { return true },
-	}
-
-	// Create a handler function for the WebSocket endpoint
-	r.GET("/ws", func(c *gin.Context) {
-		// Upgrade the HTTP connection
-		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer conn.Close()
-
-		// Start listening for messages
-		for {
-			// Read a message from the client
-			_, msg, err := conn.ReadMessage()
-			if err != nil {
-				fmt.Println(err)
-				break;
-			}
-			fmt.Println(string(msg))
-
-			// Send a message back to the client
-			if err = conn.WriteMessage(websocket.TextMessage, []byte("Hello from the server!")); err != nil {
-				fmt.Println(err)
-				break
-			}
-		}
+	server := socketio.NewServer(nil)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	server.On("connection", func(so socketio.Socket) {
+		fmt.Println("on connection")
+		so.Join("chat")
+		so.On("chat message", func(msg string) {
+			fmt.Println("emit:", so.Emit("chat message", msg))
+			so.BroadcastTo("chat", "chat message", msg)
+		})
+		so.On("disconnection", func() {
+			fmt.Println("on disconnect")
+		})
 	})
+	server.OnConnect("error", func(so socketio.Socket, err error) {
+		fmt.Println("error:", err)
+	})
+	r.GET("/socket.io/*any", gin.WrapH(server))
+	r.POST("/socket.io/*any", gin.WrapH(server))
 
+//https://github.com/googollee/go-socket.io/blob/master/_examples/gin-gonic/main.go
 }
