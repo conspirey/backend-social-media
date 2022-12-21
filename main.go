6d96ac9a5ecb0374246c0896909b0a8ccfde8664
mrredo <mrredogt@gmail.com>
2022-12-21 16:40:43 +0200

    r

--- main.go
+++ main.go
@@ -10,5 +10,10 @@ var (
 func main() {
 	r.Static("/static", "./static")
 	routes.LoadWebSocket(r);
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "hello")
+	})
 	r.Run("localhost:8080")
+
 }
--- routes/ws.go
+++ routes/ws.go
@@ -16,18 +16,21 @@ func LoadWebSocket(r *gin.Engine) {
 	// 	fmt.Print(request.Endpoint)
 	// 	return f.NewSuccessMessage(request.Message.Text)
 	//   })
+	
 	f.OnConnect(func(client *f.Client, request *f.Request) {
 		// f.Broadcast()
 		client.Join("test")
+		f.Broadcast("", "echo", f.NewSuccessMessage("hellkyrdyjrsjyjsryjsryjsrsyrzjo"))
 		fmt.Println("connected to client")
 		
 	})
 	f.Listen("echo", func(client *f.Client, request *f.Request) *f.Message {
-		f.Broadcast("test", request.Endpoint, f.NewSuccessMessage("hello"))
+		
 		fmt.Print(client.Rooms)
 		
 		return f.NewSuccessMessage("j")
 	})
+	fmt.Println("hello")
 	f.Startup(map[string]interface{}{
 		"port": 3000})
 }
