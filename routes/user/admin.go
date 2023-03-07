811f901346f185a4fef4a323856424dfaa1115c0
mrredo <mrredogt@gmail.com>
2023-03-07 08:23:25 +0200

    set admin is done but needs more error checking and returned data

--- routes/user/admin.go
+++ routes/user/admin.go
@@ -2,10 +2,13 @@ package user
 
 import (
 	"fmt"
+	mongof "main/functions/mongo"
 	"main/functions/sessions"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
@@ -17,12 +20,16 @@ PATCH REQUEST
 QUERIES:
 admin=true|false
 JSON BODY:
-{
-	"pass": "{PASSWORD}"
-}
+
+	{
+		"pass": "{PASSWORD}"
+	}
 */
 type PassStruct struct {
 	Pass string `json:"pass"`
+}
+func BoolStr(boolean string) bool {
+	return boolean == "true"
 }
 func SetAdmin(c *gin.Context, db *mongo.Database) {
 	passStr := &PassStruct{}
@@ -33,7 +40,7 @@ func SetAdmin(c *gin.Context, db *mongo.Database) {
 
 		if adminQuery != "true" && adminQuery != "false" {
 			c.JSON(400, gin.H{
-				"error": "admin_query_not_valid", 
+				"error":         "admin_query_not_valid",
 				"error_message": "Admin query has to be true|false",
 			})
 			return
@@ -41,15 +48,23 @@ func SetAdmin(c *gin.Context, db *mongo.Database) {
 		if err := c.ShouldBindJSON(&passStr); err != nil {
 			c.JSON(400, gin.H{
 				"error_message": "invalid json provided",
-				"error": "invalid_json",
+				"error":         "invalid_json",
 			})
 			return
 		}
-		fmt.Println(passStr)
+		user := session.Get("user").(map[string]any)
+		_, err := mongof.UpdateOne(bson.M{
+			"$set": bson.M{
+				"admin": BoolStr(adminQuery),
+			},
+		}, bson.M{
+			"id": user["id"].(string),
+		}, options.Update(), db, "user")
+		fmt.Println(err)
 		// if passQuery != AdminPass {
 		// 	fmt.Println(adminQuery, passQuery + "QUERY", AdminPass + "ADMIN")
 		// 	c.JSON(400, gin.H{
-		// 		"error": "password_not_valid", 
+		// 		"error": "password_not_valid",
 		// 		"error_message": "Password is invalid",
 		// 	})
 		// 	return
@@ -57,7 +72,7 @@ func SetAdmin(c *gin.Context, db *mongo.Database) {
 		c.Status(200)
 	} else {
 		c.JSON(401, gin.H{
-			"error": "user_not_logged_in", 
+			"error":         "user_not_logged_in",
 			"error_message": "User is not logged in",
 		})
 	}
