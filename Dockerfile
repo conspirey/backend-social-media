949d99981a1de226038b38f06857cc1cd49bd57c
mrredo <74524695+mrredo@users.noreply.github.com>
2025-02-20 09:58:01 +0200

    Docker support added

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM golang:1.19 as build
+WORKDIR /app
+COPY go.mod go.sum ./
+RUN go mod download
+COPY . .
+RUN go build -o main .
+
+FROM node:20 AS frontend-build
+WORKDIR /app/frontend
+COPY ./frontend ./
+RUN npm install && npm run build
+
+
+
+FROM ubuntu:latest
+LABEL authors="rihar"
+WORKDIR /app
+
+COPY .env /app/.env
+
+RUN apt update && apt install -y ca-certificates && rm -rf /var/lib/apt/lists/*
+
+# Copy the Go binary from the build stage
+COPY --from=build /app/main /app/main
+
+# Copy the frontend build from the frontend build stage
+COPY --from=frontend-build /app/frontend/build /app/frontend/build
+
+ENV PORT=313
+
+# Expose the port
+EXPOSE $PORT
+CMD ["./main"]
+
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"context"
 	"encoding/gob"
+	"github.com/joho/godotenv"
 	"os"
 	"strings"
-	"github.com/joho/godotenv"
 
 	"log"
 	"main/functions"
@@ -23,33 +23,34 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
 var (
 	keypair = functions.RandStringRunes(32)
 )
 
 func main() {
 	// gin.SetMode(gin.DebugMode)
-	
+
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Some error occured. Err: %s", err)
 	}
-	r := gin.Default();
+	r := gin.Default()
 	gob.Register(map[string]interface{}{})
 	gob.Register(map[interface{}]interface{}{})
 	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
 	clientOptions := options.Client().
-	ApplyURI(os.Getenv("MONGO")).
-    // ApplyURI("mongodb+srv://redobot:dbuserpassword123@cluster0.rhc8q.mongodb.net/Cluster0?retryWrites=true&w=majority").
-    SetServerAPIOptions(serverAPIOptions)
+		ApplyURI(os.Getenv("MONGO")).
+		// ApplyURI("mongodb+srv://redobot:dbuserpassword123@cluster0.rhc8q.mongodb.net/Cluster0?retryWrites=true&w=majority").
+		SetServerAPIOptions(serverAPIOptions)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
-    	log.Fatal(err)
+		log.Fatal(err)
 	}
 	var db = client.Database("conspir_test")
-	if len(os.Args) > 1  {
+	if len(os.Args) > 1 {
 		if os.Args[2] == "true" {
 			db = client.Database("conspir")
 		}
@@ -60,14 +61,13 @@ func main() {
 
 	r.Use(cors.New(cors.Config{
 		//  AllowAllOrigins: true,
-		AllowWildcard: true,
-		AllowWebSockets: true,
+		AllowWildcard:          true,
+		AllowWebSockets:        true,
 		AllowBrowserExtensions: true,
-		AllowOrigins: []string{"http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5173","https://127.0.0.1:443","http://127.0.0.1:80", "http://192.168.8.114:5173", "https://conspirey.xyz", "http://vm5.spacerv.ovh:3623/", "http://localhost:3623", "http://127.0.0.1:3623"},
-		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
-		AllowHeaders: []string{"Origin","Content-Length", "Content-Type", "Accept", "Cookie", "Set-Cookie"},
-		AllowCredentials: true,
-		
+		AllowOrigins:           []string{"http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5173", "https://127.0.0.1:443", "http://127.0.0.1:80", "http://192.168.8.114:5173", "https://conspirey.xyz", "http://vm5.spacerv.ovh:3623/", "http://localhost:3623", "http://127.0.0.1:3623"},
+		AllowMethods:           []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:           []string{"Origin", "Content-Length", "Content-Type", "Accept", "Cookie", "Set-Cookie"},
+		AllowCredentials:       true,
 	}))
 	r.Use(mses.MiddleWare("user", keypair, 3600*48, "", "/", false, false, http.SameSiteNoneMode))
 	// cs := db.Collection("sessions")
@@ -75,25 +75,21 @@ func main() {
 
 	// r.Use(sessions.Sessions("mysession", store))
 
-
 	server := routes.LoadWebSocket(r, keypair)
 	routes.LoadRoutes(r, db, server)
 
-	
 	r.Static("/assets/", "./frontend/dist/assets")
 	r.Static("/static/", "./frontend/dist")
 	r.GET("/socket.io/", func(c *gin.Context) {
 		RunHTTPHandler(server, c)
 	})
-	
+
 	r.POST("/socket.io/", gin.WrapH(server))
 	//r.GET("/socket.io/", gin.WrapH(f))
-
 
 	r.NoRoute(func(c *gin.Context) {
 		c.File("./frontend/dist/index.html")
 	})
-
 
 	// if os.Getenv("GIN_MODE") == "release" {
 	// 	listener, err := net.Listen("tcp4", "0.0.0.0:3200")
@@ -108,21 +104,21 @@ func main() {
 	// 	fmt.Fprintln(os.Stderr, err)
 	// 	os.Exit(1)
 	//   }
-	
-	if len(os.Args) > 1  {
+
+	if len(os.Args) > 1 {
 		if strings.ToLower(os.Args[1]) == "release" {
 			gin.SetMode(gin.ReleaseMode)
-			r.RunTLS(":3100", "./ssl/cert.pem", "./ssl/keys.pem")
+			r.RunTLS(":"+os.Getenv("PORT"), "./ssl/cert.pem", "./ssl/keys.pem")
 		} else {
 			gin.SetMode(gin.DebugMode)
-			r.Run("localhost:3100")
+			r.Run("localhost:" + os.Getenv("PORT"))
 		}
-		
+
 	} else {
 		gin.SetMode(gin.DebugMode)
-		r.Run("localhost:3100")
+		r.Run(":" + os.Getenv("PORT"))
 	}
-	
+
 	// r.Run(":3100")
 	//defer f.Shutdown()
 }
@@ -134,20 +130,19 @@ func RunHTTPHandler(h http.Handler, c *gin.Context) {
 // 	var rootCMD = &cobra.Command{
 // 		Use: "conspirey",
 // 		Short: "Help is to help you",
-	
+
 // 		Run: func(c *cobra.Command, args []string) {
-			
+
 // 			if args[0] == "release" {
 // 				listener, err := net.Listen("tcp4", "0.0.0.0:3200")
 // 				if err != nil {
-					
+
 // 				}
 // 				r.RunListener(listener)
 // 			} else {
 // 				r.Run(":3200")
 // 			}
-			
-	
+
 // 		},
 // 	}
 // 	return rootCMD
